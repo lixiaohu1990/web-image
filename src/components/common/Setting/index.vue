e3e62234963edbdef85c45b8c8b49a42a765737e
p_xhulli <p_xhulli@tencent.com>
2023-04-14 22:16:15 +0800

    fix build error

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -39,15 +39,6 @@ const show = computed({
   <NModal v-model:show="show" :auto-focus="false" preset="card" style="width: 95%; max-width: 640px">
     <div>
       <NTabs v-model:value="active" type="line" animated>
-        <!-- <NTabPane name="General" tab="General">
-          <template #tab>
-            <SvgIcon class="text-lg" icon="ri:file-user-line" />
-            <span class="ml-2">{{ $t('setting.general') }}</span>
-          </template>
-          <div class="min-h-[100px]">
-            <General />
-          </div>
-        </NTabPane> -->
         <NTabPane v-if="isChatGPTAPI" name="Advanced" tab="Advanced">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:equalizer-line" />
@@ -57,33 +48,7 @@ const show = computed({
             <Advanced />
           </div>
         </NTabPane>
-        <!-- <NTabPane name="Config" tab="Config">
-          <template #tab>
-            <SvgIcon class="text-lg" icon="ri:list-settings-line" />
-            <span class="ml-2">{{ $t('setting.config') }}</span>
-          </template>
-          <About />
-        </NTabPane> -->
       </NTabs>
     </div>
   </NModal>
 </template>
-
-
-<!-- 这段代码是 Vue 3 的一个组件，使用了<script setup>语法。它引入了一些来自 Vue、naive-ui（一个 Vue 3 UI 库）的组件和其他自定义组件。此组件主要用于显示一个弹出窗口（模态框），包含三个选项卡：General（通用），Advanced（高级）和Config（配置）。
-
-以下是代码的分析：
-
-从 Vue 中导入computed和ref函数。
-从 naive-ui 导入NModal、NTabPane和NTabs组件。
-从相对路径导入General、Advanced和About组件。
-从@/store导入useAuthStore。
-导入自定义SvgIcon组件。
-定义Props和Emit接口。
-使用defineProps和defineEmits函数定义组件的props和emit。
-使用useAuthStore来创建一个authStore实例。
-计算属性isChatGPTAPI，用于判断用户是否拥有 ChatGPT API 的权限。
-创建一个ref实例active，用于存储当前活动的选项卡。
-使用计算属性show处理模态框的显示和隐藏。
-使用<template>语法编写组件的 HTML 结构。 -->
-  <!-- 如果你想去掉“设置.配置”这一项，只需删除与NTabPane相关的代码段。以下是修改后的代码： -->
