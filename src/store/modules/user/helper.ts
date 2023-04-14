5a744aca1ba5501b6f35bfe9fb48bc0c98a047e2
p_xhulli <p_xhulli@tencent.com>
2023-04-14 19:52:02 +0800

    fix

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://duckgpt-1256187161.cos.ap-nanjing.myqcloud.com/avatar.jpg',
+      name: '小黄鸭AI助手',
+      description: 'Star on <a href="https://duckgpt-1256187161.cos.ap-nanjing.myqcloud.com/group.jpg" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
